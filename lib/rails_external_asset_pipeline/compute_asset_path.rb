48ac664ba4c818f18f5e48c0c5e6ef780fb7a4fd
Lucas Dohmen <lucas@dohmen.io>
2016-07-22 15:44:09 +0200

    Implemented functionality for stylesheets

--- lib/rails_external_asset_pipeline/compute_asset_path.rb
+++ lib/rails_external_asset_pipeline/compute_asset_path.rb
@@ -1,8 +1,10 @@
 module RailsExternalAssetPipeline
   module ComputeAssetPath
+    TYPES_WITH_MANIFEST = %i(stylesheet image)
+
     def compute_asset_path(source, options = {})
-      if options[:type] == :image
-        manifest = JSON.parse(File.read("#{Rails.root}/public/assets/manifests/image.json"))
+      if TYPES_WITH_MANIFEST.include? options[:type]
+        manifest = JSON.parse(File.read("#{Rails.root}/public/assets/manifests/#{options[:type]}.json"))
         manifest.fetch(source)
       else
         source
--- test/rails_external_asset_pipeline_test.rb
+++ test/rails_external_asset_pipeline_test.rb
@@ -7,4 +7,11 @@ class RailsExternalAssetPipeline::Test < ActionDispatch::IntegrationTest
 
     assert_equal "/assets/images/magic-f67894a08006a2af9f5076180676323c.gif", image_src
   end
+
+  def test_that_the_href_of_a_stylesheet_is_set_correctly
+    get "/fancy/index"
+    style_src = css_select("link")[0]["href"]
+
+    assert_equal "/assets/stylesheets/application-12fcfd0548a6a948c16661f06d20046f.css", style_src
+  end
 end
