c2931a910359db9bbf0d6b87f32ed1eb6cb02692
Lucas Dohmen <lucas@dohmen.io>
2016-07-22 16:40:18 +0200

    Prepare test to be able to use different assets

--- test/rails_external_asset_pipeline_test.rb
+++ test/rails_external_asset_pipeline_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
+require "fileutils"
 
 class RailsExternalAssetPipeline::Test < ActionDispatch::IntegrationTest
   def test_that_the_source_of_an_image_is_set_correctly
+    use_assets_fixtures "good_assets"
     get "/fancy/index"
     image_src = css_select("img")[0]["src"]
 
@@ -9,6 +11,7 @@ class RailsExternalAssetPipeline::Test < ActionDispatch::IntegrationTest
   end
 
   def test_that_the_href_of_a_stylesheet_is_set_correctly
+    use_assets_fixtures "good_assets"
     get "/fancy/index"
     style_src = css_select("link")[0]["href"]
 
@@ -16,6 +19,7 @@ class RailsExternalAssetPipeline::Test < ActionDispatch::IntegrationTest
   end
 
   def test_that_the_src_of_a_script_tag_is_set_correctly
+    use_assets_fixtures "good_assets"
     get "/fancy/index"
     script_src = css_select("script")[0]["src"]
 
@@ -23,10 +27,21 @@ class RailsExternalAssetPipeline::Test < ActionDispatch::IntegrationTest
   end
 
   def test_raises_an_error_when_an_asset_is_not_in_the_manifest
+    use_assets_fixtures "good_assets"
     err = assert_raises(ActionView::Template::Error) do
       get "/broken/index"
     end
 
     assert_equal err.message, "The asset 'missing.gif' of type 'image' was not in the manifest", "Check for descriptive error message"
+  end
+
+  def teardown
+    FileUtils.remove_dir "test/dummy/public/assets"
+  end
+
+  private
+
+  def use_assets_fixtures(name)
+    FileUtils.cp_r "test/fixtures/#{name}", "test/dummy/public/assets"
   end
 end
